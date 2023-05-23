2b6b1b06ba0542cb513c875fb27964708b1bf86c
Tifany Rosas <rosastifany@gmail.com>
2023-05-23 16:37:44 +0100

    Create board and square component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,39 @@
 import React, { StrictMode } from "react";
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+function Square() {
+
+  const [value, setValue] = useState(null); 
+
+  function handleClikc() {
+    setValue("X");
+  }
+
+  return (<button 
+    className="square" 
+    onClick={handleClikc}
+    >{value}</button>
+    );
+}
+
+export default function Board() {
+  return (
+    <div>
+      <div className="board-row">
+        <Square/>
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+    );
 }
